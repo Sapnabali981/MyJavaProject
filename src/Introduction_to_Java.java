f95dbcaa022708904e06ec8552a8fbdd014a4ada
Sapnabali981 <sapnabali45@gmail.com>
2023-12-25 15:30:51 +0530

    Initial commit

---
+++ src/Introduction_to_Java.java
@@ -0,0 +1,24 @@
+
+public class Introduction_to_Java {
+	
+	public static void main(String[] args) {
+		/*
+		 * basically java was developed by sun microsystem in the year 1995 and led by team of james gosling
+		 * later on it belongs to oracle.
+		 * in order to check whether java is existing in our local system or not for that we can make use of several commands
+		 * java --version
+		 * in order to check the java complier version we can use commands
+		 * javac --version
+		 * creating a program in order to print Hello world in the console
+		 * jshell java interpreter is one of the most feature which is been introduced in java 8 
+		 * with the help of jshell we can write certain line of codes for experimenting purpose.
+		 * whenever a java program executes they are two major important steps which occur behind the scene
+		 * 1.compilation :- here complier converts java code converts into bytecode
+		 * in order to compile the code we make use of command javac javafilename.java
+		 * 2.execution :-then jvm converts bytecode into machine code 
+		 * in order to run the code we make use of command java javafilename.java
+		 */
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/Variables.java
@@ -0,0 +1,30 @@
+
+public class Variables {
+	
+	public static void main(String[] args) {
+		/*
+		 * as a programmer when we write any java code
+		 * compiler will comiple the code we get bytecode
+		 * again bytecode is executed by jvm in order to get desired output in the console.
+		 * JVM will execute the bytecode only if it has main method in it.
+		 * why we build the software..?
+		 * we build software in order to solve the real problems with the help of virtual solutions
+		 * so while building java application we usually process data 
+		 * while processing data of user we generally store it in permanent or temprary way
+		 * for permanent storage we will make use of data base
+		 * for temparary storage we will make use of variables
+		 * so here variables are basically placeholder which hold certains data in it.
+		 * variables contains two main components in it
+		 * name and values
+		 * while declaring the variables in java we make use of data type
+		 * data type will define the classification of data like what values can be stored and what operation can be performed using those data can be identified with the help of data type
+		 * that's why java is called as strongly typed programming language
+		 */
+		//creating variables and performing addition operation 
+		int num1=12;
+		int num2=13;
+		int result=num1+num2;
+		System.out.println("The addition of num1 and num2 is:"+result);
+	}
+
+}
